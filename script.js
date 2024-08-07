1047c2430787211dc35856c73f9ed9a33d92c2f0
mufafa9 <mustafa20122022@gmail.com>
2024-08-07 20:27:23 +0300

    sorry

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let output;
 let input;
 
-document.getElementById("btn").onclick= function(){
+document.getElementById("mybtn").onclick= function(){
    input= Document.getElementById("input").value;
    output=input * 1.609;
    document.getElementById("h2").textContent=output;
 };
+console.log("")
